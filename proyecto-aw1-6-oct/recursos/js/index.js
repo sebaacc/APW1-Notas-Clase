adec643a7c141163da4c72a890fb51364c608f76
sebaacc <sebaacc11@gmail.com>
2025-10-06 14:44:52 -0300

    clase 6 oct js

--- proyecto-aw1-6-oct/recursos/js/index.js
+++ proyecto-aw1-6-oct/recursos/js/index.js
@@ -1,7 +1,8 @@
 // let y const son tipos, que pueden establecer una variable o una constante.
 /*
     el nombre de una variable es un IDENTIFICADOR. 
-    JavaScript o JS es de débil tipado, y dinámico ya que una variable con let puede cambiar su tipado.
+    JavaScript o JS es de débil tipado porque no es necesario indicar un tipo, y dinámico ya que una variable puede cambiar su tipo a la hora de reemplazar su contenido.
+    Es un lenguaje interpretado. Se traduce en tiempo de ejecución.
 */  
 let nombre = "seba"
 
@@ -10,5 +11,36 @@ const apellido = "markoja"
 // nombre = "sebinha" antes del console log se puede cambiar nuevamente la variable.
 // apellido = "markouja" esto da error ya que es una constante.
 
-console.log(nombre, apellido)
+//console.log(nombre, apellido)
 
+/*
+console.log("10 y 5");
+
+console.log("suma")
+console.log(10+5)
+console.log("resta")
+console.log(10-5)
+console.log("división");
+console.log(10/5)
+console.log("módulo");
+console.log(10%5)
+*/
+
+/*
+const suma = 15+5
+console.log(suma);
+
+let nombrecompleto = nombre + ' ' + apellido
+console.log(nombrecompleto);
+
+*/
+
+const estoyFeliz = true
+const mayorQue = 10>5
+
+console.log(estoyFeliz, mayorQue);
+
+/* 
+    Expresión: es una operacion o algo que luego javascript tiene que realizar o resolver.
+    Valor: el valor de la variable, sea lo que se declaró antes, o el resultado final de la expresión.
+*/
