7bb2644dc0c37ffd0e934bc3ad388b5ebab74526
sebaacc <sebaacc11@gmail.com>
2025-10-06 14:24:07 -0300

    clase JAVASCRIPT 6 oct

---
+++ proyecto-aw1-6-oct/recursos/js/index.js
@@ -0,0 +1,14 @@
+// let y const son tipos, que pueden establecer una variable o una constante.
+/*
+    el nombre de una variable es un IDENTIFICADOR. 
+    JavaScript o JS es de débil tipado, y dinámico ya que una variable con let puede cambiar su tipado.
+*/  
+let nombre = "seba"
+
+const apellido = "markoja"
+
+// nombre = "sebinha" antes del console log se puede cambiar nuevamente la variable.
+// apellido = "markouja" esto da error ya que es una constante.
+
+console.log(nombre, apellido)
+
