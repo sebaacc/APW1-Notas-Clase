fc09b8232d10de2d6deb3981567bf789dd831871
sebaacc <sebaacc11@gmail.com>
2025-10-07 13:43:55 -0300

    clase 7 oct

--- proyecto-aw1-6-oct/recursos/js/index.js
+++ proyecto-aw1-6-oct/recursos/js/index.js
@@ -1,9 +1,14 @@
 // let y const son tipos, que pueden establecer una variable o una constante.
-/*
-    el nombre de una variable es un IDENTIFICADOR. 
-    JavaScript o JS es de débil tipado porque no es necesario indicar un tipo, y dinámico ya que una variable puede cambiar su tipo a la hora de reemplazar su contenido.
-    Es un lenguaje interpretado. Se traduce en tiempo de ejecución.
-*/  
+//                  ---SP4 TID---
+//     el nombre de una variable es un IDENTIFICADOR. 
+//     JavaScript o JS es de débil tipado porque no es necesario indicar un tipo, y dinámico ya que una variable puede cambiar su tipo a la hora de reemplazar su contenido.
+//     Es un lenguaje interpretado. Se traduce en tiempo de ejecución.
+
+//     Hoy practicamos operaciones:
+//     - Aritméticas
+//     - De cadena
+//     - Lógicos
+
 let nombre = "seba"
 
 const apellido = "markoja"
@@ -11,9 +16,10 @@ const apellido = "markoja"
 // nombre = "sebinha" antes del console log se puede cambiar nuevamente la variable.
 // apellido = "markouja" esto da error ya que es una constante.
 
-//console.log(nombre, apellido)
 
 /*
+console.log(nombre, apellido)
+
 console.log("10 y 5");
 
 console.log("suma")
@@ -35,12 +41,44 @@ console.log(nombrecompleto);
 
 */
 
-const estoyFeliz = true
-const mayorQue = 10>5
+// const estoyFeliz = true
+// const mayorQue = 10>5
 
-console.log(estoyFeliz, mayorQue);
+// console.log(estoyFeliz, mayorQue);
 
-/* 
-    Expresión: es una operacion o algo que luego javascript tiene que realizar o resolver.
-    Valor: el valor de la variable, sea lo que se declaró antes, o el resultado final de la expresión.
-*/
+
+//  - Expresión: es una operacion o algo que luego javascript tiene que realizar o resolver.
+//  - Valor: el valor de la variable, sea lo que se declaró antes, o el resultado final de la expresión.
+
+
+//Backticks ` `
+
+// Estructuras de Control: de flujo (condicionales) y los bucles.
+
+// const edad = 18
+// let mayoriaDeEdad = 18
+
+// mayoriaDeEdad = 18
+
+// if (edad == mayoriaDeEdad) { 
+//     // console.log("Tiene exactamente " + mayoriaDeEdad);
+//     console.log(`Tiene exactamente ${edad}`); // => esto es TEMPLATE STRINGS o STRING LITERALS. PÁGINA 331 TID
+
+// } else if (edad > mayoriaDeEdad) {
+//     console.log("Es mayor de edad");
+// } 
+// else {
+//     console.log("No es mayor de edad");
+// }
+
+
+// let veces = 60
+
+// for (let i = 1; i<veces; i++) {
+//     if (i == 1)  console.log(`${i} vez`)
+//     else console.log(`${i} veces`)
+// }
+
+// console.log(1 === "1", 1 == "1"); //compara tipo, compara solo contenido 
+
+
