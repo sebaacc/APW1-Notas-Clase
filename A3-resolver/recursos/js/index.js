f968dfe4d5dc6ec12f0438ad402d3a823e2afe56
sebaacc <sebaacc11@gmail.com>
2025-11-04 13:11:04 -0300

    hasta punto 3

---
+++ A3-resolver/recursos/js/index.js
@@ -0,0 +1,8 @@
+import {renderizadoInmueble} from "./vista.js"
+
+let contenedor = document.getElementById("contenedor")
+
+/*
+contenedor.innerHTML = "Hola contenedor"
+*/
+renderizadoInmueble(contenedor)
--- A3-resolver/recursos/js/vista.js
+++ A3-resolver/recursos/js/vista.js
@@ -1,1 +1,26 @@
 // Funciones renderizado
+
+function renderizadoInmueble(contenedor) {
+
+    contenedor.innerHTML = ` <article class="catalogo-inmueble">
+<img
+    src="./recursos/items/depto-2.jpg"
+    alt="Departamento 1"
+/>
+<h3>Departamento1</h3>
+<ul>
+    <li>Operación: Alquiler</li>
+    <li>Precio: $ 1000</li>
+</ul>
+<button
+    class="boton-consulta"
+    data-codigo="D01"
+>
+    Consultar
+</button>
+</article> `
+}
+
+export {
+    renderizadoInmueble
+}
